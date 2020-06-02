c4914dcd8261e614acaf1f525922c946d85ab3da
Johan Cardell <Johan.Cardell@iths.se>
2020-06-02 10:53:07 +0200

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Quizzer.Data;
+using Quizzer.Models;
+
+[assembly: HostingStartup(typeof(Quizzer.Areas.Identity.IdentityHostingStartup))]
+namespace Quizzer.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
